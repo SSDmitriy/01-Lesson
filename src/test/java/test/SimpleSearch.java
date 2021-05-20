c7bfa79547cc6c6a0ca57319e9146ac3ed4ec28c
Dmitriy <xyzmail@yandex.ru>
2021-05-20 22:44:26 +0400

    add SimpleSearch

--- src/test/java/test/SimpleSearch.java
+++ src/test/java/test/SimpleSearch.java
@@ -1,4 +1,23 @@
 package test;
 
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class SimpleSearch {
+
+    @Test
+    void wikipediaSearchTest() {
+
+        //open google
+        open("https://google.com");
+
+        //type search "wikipedia.org", press Enter
+        $("[name=q]").setValue("wikipedia").pressEnter();
+
+        //check "Википедия — свободная энциклопедия" in result
+        $("#rso").shouldHave(text("wikipedia.org"));
+    }
 }
